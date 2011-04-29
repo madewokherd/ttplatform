56b20fad3c20977059a7f5ed67fdf63829668524
Vincent Povirk <madewokherd@gmail.com>
2011-04-29 02:59:55 -0500

    initial commit

---
+++ data/tiles.tsx
@@ -0,0 +1,536 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tiles" tilewidth="16" tileheight="16">
+ <image source="tiles.png" width="256" height="256"/>
+ <tile id="1">
+  <properties>
+   <property name="height" value="32"/>
+   <property name="type" value="player"/>
+   <property name="width" value="16"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="color" value="red"/>
+   <property name="type" value="button"/>
+  </properties>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="color" value="blue"/>
+   <property name="type" value="button"/>
+  </properties>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="type" value="box"/>
+  </properties>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="drawas" value="0"/>
+  </properties>
+ </tile>
+ <tile id="23">
+  <properties>
+   <property name="color" value="red"/>
+   <property name="type" value="switchground"/>
+  </properties>
+ </tile>
+ <tile id="24">
+  <properties>
+   <property name="color" value="red"/>
+   <property name="type" value="switchground"/>
+  </properties>
+ </tile>
+ <tile id="25">
+  <properties>
+   <property name="color" value="red"/>
+   <property name="type" value="switchground"/>
+  </properties>
+ </tile>
+ <tile id="26">
+  <properties>
+   <property name="color" value="red"/>
+   <property name="type" value="switchground"/>
+  </properties>
+ </tile>
+ <tile id="27">
+  <properties>
+   <property name="color" value="blue"/>
+   <property name="type" value="switchground"/>
+  </properties>
+ </tile>
+ <tile id="28">
+  <properties>
+   <property name="color" value="blue"/>
+   <property name="type" value="switchground"/>
+  </properties>
+ </tile>
+ <tile id="29">
+  <properties>
+   <property name="color" value="blue"/>
+   <property name="type" value="switchground"/>
+  </properties>
+ </tile>
+ <tile id="30">
+  <properties>
+   <property name="color" value="blue"/>
+   <property name="type" value="switchground"/>
+  </properties>
+ </tile>
+ <tile id="32">
+  <properties>
+   <property name="type" value="ground"/>
+  </properties>
+ </tile>
+ <tile id="33">
+  <properties>
+   <property name="type" value="ground"/>
+  </properties>
+ </tile>
+ <tile id="34">
+  <properties>
+   <property name="type" value="ground"/>
+  </properties>
+ </tile>
+ <tile id="35">
+  <properties>
+   <property name="type" value="ground"/>
+  </properties>
+ </tile>
+ <tile id="36">
+  <properties>
+   <property name="height" value="32"/>
+   <property name="type" value="exit"/>
+   <property name="width" value="16"/>
+  </properties>
+ </tile>
+ <tile id="39">
+  <properties>
+   <property name="color" value="red"/>
+   <property name="type" value="switchground"/>
+  </properties>
+ </tile>
+ <tile id="40">
+  <properties>
+   <property name="color" value="red"/>
+   <property name="type" value="switchground"/>
+  </properties>
+ </tile>
+ <tile id="41">
+  <properties>
+   <property name="color" value="red"/>
+   <property name="type" value="switchground"/>
+  </properties>
+ </tile>
+ <tile id="42">
+  <properties>
+   <property name="color" value="red"/>
+   <property name="type" value="switchground"/>
+  </properties>
+ </tile>
+ <tile id="43">
+  <properties>
+   <property name="color" value="blue"/>
+   <property name="type" value="switchground"/>
+  </properties>
+ </tile>
+ <tile id="44">
+  <properties>
+   <property name="color" value="blue"/>
+   <property name="type" value="switchground"/>
+  </properties>
+ </tile>
+ <tile id="45">
+  <properties>
+   <property name="color" value="blue"/>
+   <property name="type" value="switchground"/>
+  </properties>
+ </tile>
+ <tile id="46">
+  <properties>
+   <property name="color" value="blue"/>
+   <property name="type" value="switchground"/>
+  </properties>
+ </tile>
+ <tile id="48">
+  <properties>
+   <property name="type" value="ground"/>
+  </properties>
+ </tile>
+ <tile id="49">
+  <properties>
+   <property name="type" value="ground"/>
+  </properties>
+ </tile>
+ <tile id="50">
+  <properties>
+   <property name="type" value="ground"/>
+  </properties>
+ </tile>
+ <tile id="51">
+  <properties>
+   <property name="type" value="ground"/>
+  </properties>
+ </tile>
+ <tile id="55">
+  <properties>
+   <property name="color" value="red"/>
+   <property name="type" value="switchground"/>
+  </properties>
+ </tile>
+ <tile id="56">
+  <properties>
+   <property name="color" value="red"/>
+   <property name="type" value="switchground"/>
+  </properties>
+ </tile>
+ <tile id="57">
+  <properties>
+   <property name="color" value="red"/>
+   <property name="type" value="switchground"/>
+  </properties>
+ </tile>
+ <tile id="58">
+  <properties>
+   <property name="color" value="red"/>
+   <property name="type" value="switchground"/>
+  </properties>
+ </tile>
+ <tile id="59">
+  <properties>
+   <property name="color" value="blue"/>
+   <property name="type" value="switchground"/>
+  </properties>
+ </tile>
+ <tile id="60">
+  <properties>
+   <property name="color" value="blue"/>
+   <property name="type" value="switchground"/>
+  </properties>
+ </tile>
+ <tile id="61">
+  <properties>
+   <property name="color" value="blue"/>
+   <property name="type" value="switchground"/>
+  </properties>
+ </tile>
+ <tile id="62">
+  <properties>
+   <property name="color" value="blue"/>
+   <property name="type" value="switchground"/>
+  </properties>
+ </tile>
+ <tile id="64">
+  <properties>
+   <property name="type" value="ground"/>
+  </properties>
+ </tile>
+ <tile id="65">
+  <properties>
+   <property name="type" value="ground"/>
+  </properties>
+ </tile>
+ <tile id="66">
+  <properties>
+   <property name="type" value="ground"/>
+  </properties>
+ </tile>
+ <tile id="67">
+  <properties>
+   <property name="type" value="ground"/>
+  </properties>
+ </tile>
+ <tile id="71">
+  <properties>
+   <property name="color" value="red"/>
+   <property name="type" value="switchground"/>
+  </properties>
+ </tile>
+ <tile id="72">
+  <properties>
+   <property name="color" value="red"/>
+   <property name="type" value="switchground"/>
+  </properties>
+ </tile>
+ <tile id="73">
+  <properties>
+   <property name="color" value="red"/>
+   <property name="type" value="switchground"/>
+  </properties>
+ </tile>
+ <tile id="74">
+  <properties>
+   <property name="color" value="red"/>
+   <property name="type" value="switchground"/>
+  </properties>
+ </tile>
+ <tile id="75">
+  <properties>
+   <property name="color" value="blue"/>
+   <property name="type" value="switchground"/>
+  </properties>
+ </tile>
+ <tile id="76">
+  <properties>
+   <property name="color" value="blue"/>
+   <property name="type" value="switchground"/>
+  </properties>
+ </tile>
+ <tile id="77">
+  <properties>
+   <property name="color" value="blue"/>
+   <property name="type" value="switchground"/>
+  </properties>
+ </tile>
+ <tile id="78">
+  <properties>
+   <property name="color" value="blue"/>
+   <property name="type" value="switchground"/>
+  </properties>
+ </tile>
+ <tile id="80">
+  <properties>
+   <property name="type" value="ground"/>
+  </properties>
+ </tile>
+ <tile id="81">
+  <properties>
+   <property name="type" value="ground"/>
+  </properties>
+ </tile>
+ <tile id="82">
+  <properties>
+   <property name="type" value="ground"/>
+  </properties>
+ </tile>
+ <tile id="83">
+  <properties>
+   <property name="type" value="ground"/>
+  </properties>
+ </tile>
+ <tile id="87">
+  <properties>
+   <property name="color" value="red"/>
+   <property name="invert" value=""/>
+   <property name="type" value="switchground"/>
+  </properties>
+ </tile>
+ <tile id="88">
+  <properties>
+   <property name="color" value="red"/>
+   <property name="invert" value=""/>
+   <property name="type" value="switchground"/>
+  </properties>
+ </tile>
+ <tile id="89">
+  <properties>
+   <property name="color" value="red"/>
+   <property name="invert" value=""/>
+   <property name="type" value="switchground"/>
+  </properties>
+ </tile>
+ <tile id="90">
+  <properties>
+   <property name="color" value="red"/>
+   <property name="invert" value=""/>
+   <property name="type" value="switchground"/>
+  </properties>
+ </tile>
+ <tile id="103">
+  <properties>
+   <property name="color" value="red"/>
+   <property name="invert" value=""/>
+   <property name="type" value="switchground"/>
+  </properties>
+ </tile>
+ <tile id="104">
+  <properties>
+   <property name="color" value="red"/>
+   <property name="invert" value=""/>
+   <property name="type" value="switchground"/>
+  </properties>
+ </tile>
+ <tile id="105">
+  <properties>
+   <property name="color" value="red"/>
+   <property name="invert" value=""/>
+   <property name="type" value="switchground"/>
+  </properties>
+ </tile>
+ <tile id="106">
+  <properties>
+   <property name="color" value="red"/>
+   <property name="invert" value=""/>
+   <property name="type" value="switchground"/>
+  </properties>
+ </tile>
+ <tile id="119">
+  <properties>
+   <property name="color" value="red"/>
+   <property name="invert" value=""/>
+   <property name="type" value="switchground"/>
+  </properties>
+ </tile>
+ <tile id="120">
+  <properties>
+   <property name="color" value="red"/>
+   <property name="invert" value=""/>
+   <property name="type" value="switchground"/>
+  </properties>
+ </tile>
+ <tile id="121">
+  <properties>
+   <property name="color" value="red"/>
+   <property name="invert" value=""/>
+   <property name="type" value="switchground"/>
+  </properties>
+ </tile>
+ <tile id="122">
+  <properties>
+   <property name="color" value="red"/>
+   <property name="invert" value=""/>
+   <property name="type" value="switchground"/>
+  </properties>
+ </tile>
+ <tile id="135">
+  <properties>
+   <property name="color" value="red"/>
+   <property name="invert" value=""/>
+   <property name="type" value="switchground"/>
+  </properties>
+ </tile>
+ <tile id="136">
+  <properties>
+   <property name="color" value="red"/>
+   <property name="invert" value=""/>
+   <property name="type" value="switchground"/>
+  </properties>
+ </tile>
+ <tile id="137">
+  <properties>
+   <property name="color" value="red"/>
+   <property name="invert" value=""/>
+   <property name="type" value="switchground"/>
+  </properties>
+ </tile>
+ <tile id="138">
+  <properties>
+   <property name="color" value="red"/>
+   <property name="invert" value=""/>
+   <property name="type" value="switchground"/>
+  </properties>
+ </tile>
+ <tile id="91">
+  <properties>
+   <property name="color" value="blue"/>
+   <property name="invert" value=""/>
+   <property name="type" value="switchground"/>
+  </properties>
+ </tile>
+ <tile id="92">
+  <properties>
+   <property name="color" value="blue"/>
+   <property name="invert" value=""/>
+   <property name="type" value="switchground"/>
+  </properties>
+ </tile>
+ <tile id="93">
+  <properties>
+   <property name="color" value="blue"/>
+   <property name="invert" value=""/>
+   <property name="type" value="switchground"/>
+  </properties>
+ </tile>
+ <tile id="94">
+  <properties>
+   <property name="color" value="blue"/>
+   <property name="invert" value=""/>
+   <property name="type" value="switchground"/>
+  </properties>
+ </tile>
+ <tile id="107">
+  <properties>
+   <property name="color" value="blue"/>
+   <property name="invert" value=""/>
+   <property name="type" value="switchground"/>
+  </properties>
+ </tile>
+ <tile id="108">
+  <properties>
+   <property name="color" value="blue"/>
+   <property name="invert" value=""/>
+   <property name="type" value="switchground"/>
+  </properties>
+ </tile>
+ <tile id="109">
+  <properties>
+   <property name="color" value="blue"/>
+   <property name="invert" value=""/>
+   <property name="type" value="switchground"/>
+  </properties>
+ </tile>
+ <tile id="110">
+  <properties>
+   <property name="color" value="blue"/>
+   <property name="invert" value=""/>
+   <property name="type" value="switchground"/>
+  </properties>
+ </tile>
+ <tile id="123">
+  <properties>
+   <property name="color" value="blue"/>
+   <property name="invert" value=""/>
+   <property name="type" value="switchground"/>
+  </properties>
+ </tile>
+ <tile id="124">
+  <properties>
+   <property name="color" value="blue"/>
+   <property name="invert" value=""/>
+   <property name="type" value="switchground"/>
+  </properties>
+ </tile>
+ <tile id="125">
+  <properties>
+   <property name="color" value="blue"/>
+   <property name="invert" value=""/>
+   <property name="type" value="switchground"/>
+  </properties>
+ </tile>
+ <tile id="126">
+  <properties>
+   <property name="color" value="blue"/>
+   <property name="invert" value=""/>
+   <property name="type" value="switchground"/>
+  </properties>
+ </tile>
+ <tile id="139">
+  <properties>
+   <property name="color" value="blue"/>
+   <property name="invert" value=""/>
+   <property name="type" value="switchground"/>
+  </properties>
+ </tile>
+ <tile id="140">
+  <properties>
+   <property name="color" value="blue"/>
+   <property name="invert" value=""/>
+   <property name="type" value="switchground"/>
+  </properties>
+ </tile>
+ <tile id="141">
+  <properties>
+   <property name="color" value="blue"/>
+   <property name="invert" value=""/>
+   <property name="type" value="switchground"/>
+  </properties>
+ </tile>
+ <tile id="142">
+  <properties>
+   <property name="color" value="blue"/>
+   <property name="invert" value=""/>
+   <property name="type" value="switchground"/>
+  </properties>
+ </tile>
+</tileset>
